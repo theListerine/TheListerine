29e69ae4138273a6bf8843c49eaf873ed4ceda0a
Bjorkbat <bjorkbat@gmail.com>
2017-03-13 19:22:41 -0600

    Added basic firebase authentication

--- app/src/main/java/com/example/paullucero/thelist/MainActivity.java
+++ app/src/main/java/com/example/paullucero/thelist/MainActivity.java
@@ -1,12 +1,76 @@
 package com.example.paullucero.thelist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        } else {
+            // not signed in
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder().build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+
+                // TODO: do something once user is signed in.
+                /*
+                startActivity(SignedInActivity.createIntent(this, response));
+                finish();
+                return;
+                */
+            } else {
+
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(this,
+                            "Sign In Cancelled.  You have to sign-in in order to use this app.",
+                            Toast.LENGTH_LONG);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(this, R.string.no_internet_connection, Toast.LENGTH_LONG);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, R.string.unknown_error, Toast.LENGTH_LONG);
+                    return;
+                }
+            }
+        }
     }
 }
